393b9e6a8238602b7b771ed9bfea75b3d138e681
alexangria1992 <alex040892@gmail.com>
2023-03-22 10:22:39 +1300

    creating an api class

--- movie-explorer/src/sagas/index.js
+++ movie-explorer/src/sagas/index.js
@@ -1,8 +1,12 @@
 import { delay, all, call, put, takeEvery, takeLatest } from "redux-saga";
 import { fetchedSearchMovies } from "../redux/search";
+import { API_KEY } from "../config";
+import TheMovieDbApi from "../lib/api";
+
+const api = new TheMovieDbApi(API_KEY);
 
 function* fetchSearchMovies(action) {
   yield delay(500);
 
-  yield put(fetchedSearchMovies(yield call()));
+  yield put(fetchedSearchMovies(yield call(api.searchMovies, action.payload)));
 }
