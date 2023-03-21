562b2d3e915838a878d67a8c66f510aace48dcbf
alexangria1992 <alex040892@gmail.com>
2023-03-21 16:15:03 +1300

    Using MUI  Styled Components

--- movie-explorer/src/components/Layout.js
+++ movie-explorer/src/components/Layout.js
@@ -3,6 +3,7 @@ import { ThemeProvider, createTheme } from "@mui/material/styles";
 import { CssBaseline } from "@mui/material";
 import { Link } from "react-router-dom";
 import logoImg from "../logo.png";
+import { styled } from "@mui/system";
 
 const darkTheme = createTheme({
   palette: {
@@ -10,15 +11,33 @@ const darkTheme = createTheme({
   },
 });
 
+const Img = styled("img")({
+  marginLeft: "auto",
+  marginRight: "auto",
+  display: "block",
+  width: 500,
+  maxWidth: "100%",
+});
+
+const LayoutWrapper = styled("div")(({ theme }) => ({
+  margin: 24,
+  width: "auto",
+  [theme.breakpoints.up("lg")]: {
+    marginLeft: "auto",
+    marginRight: "auto",
+    width: theme.breakpoints.values.lg,
+  },
+}));
+
 const Layout = ({ children }) => {
   return (
     <ThemeProvider theme={darkTheme}>
       <CssBaseline />
-      <div>
+      <LayoutWrapper>
         <Link to="/">
-          <img src={logoImg} alt="" />
+          <Img src={logoImg} alt="" />
         </Link>
-      </div>
+      </LayoutWrapper>
       {children}
     </ThemeProvider>
   );
